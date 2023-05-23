8aa6dc5cca78d6a49e213e966a53d9e3ec15cb45
A.Kistanov <aleksey.kistanov@yandex.ru>
2023-05-23 21:47:12 +0500

    refacto1709r

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -13,6 +14,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@Validated
 public class UserController {
     Integer count = 1;
     private final Map<Integer, User> users = new HashMap<>();
